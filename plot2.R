3dd9f377f1a6870327f90820bb63d7b8716514ce
ntdung96 <dtnguyen@randolphcollege.edu>
2017-08-18 15:30:50 +0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,4 +6,4 @@ subset <- subset(data, date >= as.Date("2007-02-01") & date < as.Date("2007-02-0
 png(filename = "plot2.R", width = 480, height = 480)
 with(subset, plot(date, Global_active_power, xlab = "", type = "l",
                   ylab = "Global Active Power (kilowatts)"))
-devoff()
+dev.off()
